034db2481df1a29ed523ff76821af54f23afa76e
Poyraz Alkan <poyrazdaka@gmail.com>
2025-01-27 11:12:05 +0300

    servis yapıldı

--- index.js
+++ index.js
@@ -20,7 +20,11 @@
  * - filePath: Yüklenecek dosyanın yolu
  * 
  */
-
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
 const { Storage } = require('@google-cloud/storage');
 const { VertexAI } = require('@google-cloud/vertexai');
 
@@ -29,6 +33,72 @@ const project = 'vertexaimrc'; // Mevcut projemiz
 const location = 'us-central1'; 
 const storage = new Storage({ projectId: project });
 const vertexAI = new VertexAI({ project: project, location: location });
+
+
+
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+
+const app = express();
+const PORT = 8000;
+
+// Multer yapılandırması: Dosyaları 'uploads' klasörüne kaydet
+const storagelocal = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, uniqueSuffix + path.extname(file.originalname));
+  },
+});
+const upload = multer({ storage: storagelocal });
+app.get("/",async (req, res) => {
+  res.send("Google VertexAI Bridge Ayakta")
+})
+
+app.post('/upload', upload.single('file'), async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).send('Dosya yüklenmedi.');
+    }
+    await sleep(1000);
+    let ress={}
+
+    const bucketName = 'vertexaimrc-photos'; 
+    const storageClass = 'STANDARD'; 
+    const filePath = __dirname + `/uploads/${req.file.filename}` // Path
+  
+  
+    const createdBucket = await createBucketIfNotExists(bucketName, storageClass, location);
+  
+   
+    const gcsUri = await uploadFile(createdBucket, filePath);
+  
+  
+    if (gcsUri) {
+    ress =  await analyzeImage(gcsUri);
+    res.send(ress);
+    }
+    else{
+      throw "analiz yapılmadı";
+    }
+
+    
+  } catch (error) {
+    console.error('Dosya yükleme hatası:', error.message);
+    res.status(500).send('Sunucuda bir hata oluştu.');
+  }
+});
+
+// Statik dosyalar için uploads klasörünü kullanıma aç
+app.use('/uploads', express.static('uploads'));
+
+// Server başlat
+app.listen(PORT, () => {
+  console.log(`Sunucu çalışıyor: http://localhost:${PORT}`);
+});
 
 
 async function createBucketIfNotExists(bucketName, storageClass, location) {
@@ -83,25 +153,26 @@ async function analyzeImage(gcsUri) {
     }
     const aggregatedResponse = await streamingResult.response;
     console.log('Sonuç: ', aggregatedResponse.candidates[0].content.parts[0].text);
+    return aggregatedResponse;
   } catch (error) {
     console.error('Görsel analizi yapılırken hata oluştu:', error.message);
   }
 }
 
 // Ana fonksiyon
-(async function main() {
-  const bucketName = 'vertexaimrc-photos'; 
-  const storageClass = 'STANDARD'; 
-  const filePath = 'tablo.jpg'; // Path
+// (async function main() {
+//   const bucketName = 'vertexaimrc-photos'; 
+//   const storageClass = 'STANDARD'; 
+//   const filePath = 'tablo.jpg'; // Path
 
 
-  const createdBucket = await createBucketIfNotExists(bucketName, storageClass, location);
+//   const createdBucket = await createBucketIfNotExists(bucketName, storageClass, location);
 
  
-  const gcsUri = await uploadFile(createdBucket, filePath);
+//   const gcsUri = await uploadFile(createdBucket, filePath);
 
 
-  if (gcsUri) {
-    await analyzeImage(gcsUri);
-  }
-})();
+//   if (gcsUri) {
+//     await analyzeImage(gcsUri);
+//   }
+// })();
